f8cea425612174d15bab9701f0e82d9750eff878
Kövecses Levente <kovecses.levente@students.jedlik.eu>
2022-11-22 09:50:16 +0100

    menu function

--- functions.py
+++ functions.py
@@ -1,2 +1,13 @@
-from data import osszes_kony, kiberelt_konyvek
+from data import osszes_kony, kiberelt_konyvekos
+from os import system
 
+def menu():
+    system("cls")
+    print("-------------------MENÜ-------------------")
+    print("0 - kilépés")
+    print("1 - összes könyv kiírása")
+    print("2 - kibérelt könyvek kiírása")
+    print("3 - könyv kibérlése")
+    print("4 - könyv visszahozása")
+    print("5 - összes könyvből törlés")
+    print("6 - új könyv feljegyzése")
