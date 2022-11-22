1287f5208bc78429b18ee5e4f46756ba6b8df01f
Kövecses Levente <kovecses.levente@students.jedlik.eu>
2022-11-22 10:13:19 +0100

    main while ciklus

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 from functions import *
+
+bekert = -1
+
+while bekert != 0:
+    menu()
+    bekert = input()
+
+    if bekert == 1:
+        pass
+    elif bekert == 2:
+        pass
+    elif bekert == 3:
+        pass
+    elif bekert == 4:
+        pass
+    elif bekert == 5:
+        pass
+    elif bekert == 6:
+        pass
